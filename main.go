beefced10f5cc5ad897dfad74ca6dd6c833220ff
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-12 12:43:16 -0500

    Update docker-compose.yaml to add nginx service and remove unnecessary port mapping

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/SDI-Boston/filemanager_go_node/controller"
-)
-
 func main() {
 
 	// Test de conexión y creación con el servidor NFS
-	if err := controller.TestNFSConnection(); err != nil {
+	/*if err := controller.TestNFSConnection(); err != nil {
 		// Manejar el error aquí, por ejemplo, imprimirlo y salir
 		fmt.Println("Error:", err)
 		return
 	}
 
-	/*go func() {
+	go func() {
 		if err := http.ListenAndServeTLS(":5000", "cert.pem", "key.pem", router); err != nil {
 			fmt.Printf("Error al iniciar el servidor HTTPS: %s\n", err)
 		}
