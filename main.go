2bb2ccd4c08b8ec3676ac89018c0cc63cd323d41
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-13 17:49:47 -0500

    metodo upload grpc (sin testear)

--- main.go
+++ main.go
@@ -2,21 +2,4 @@ package main
 
 func main() {
 
-	// Test de conexión y creación con el servidor NFS
-	/*if err := controller.TestNFSConnection(); err != nil {
-		// Manejar el error aquí, por ejemplo, imprimirlo y salir
-		fmt.Println("Error:", err)
-		return
-	}
-
-	go func() {
-		if err := http.ListenAndServeTLS(":5000", "cert.pem", "key.pem", router); err != nil {
-			fmt.Printf("Error al iniciar el servidor HTTPS: %s\n", err)
-		}
-	}()
-
-	fmt.Println("Servidor en ejecución en https://localhost:5000")*/
-
-	//select {}
-
 }
--- server/uploadServer.go
+++ server/uploadServer.go
@@ -1,1 +1,53 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	pb "github.com/SDI-Boston/filemanager_go_node/proto"
+)
+
+type FileService struct{}
+
+func (s *FileService) Upload(ctx context.Context, req *pb.FileUploadRequest) (*pb.FileUploadResponse, error) {
+
+	// Subir archivo
+	err := uploadToNFS(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to upload file to NFS: %w", err)
+	}
+
+	// Respuesta
+	filePath := fmt.Sprintf("/mnt/nfs/%s/%s", req.OwnerId, req.FileId)
+	return &pb.FileUploadResponse{
+		FileId: req.FileId,
+		Urls:   []string{filePath},
+	}, nil
+}
+
+func uploadToNFS(req *pb.FileUploadRequest) error {
+	// Si el usuario nunca ha creado un archivo, crear un directorio para el usuario
+	userPath := fmt.Sprintf("/mnt/nfs/%s", req.OwnerId)
+	if _, err := os.Stat(userPath); os.IsNotExist(err) {
+		err := os.Mkdir(userPath, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create user directory: %w", err)
+		}
+	}
+
+	filePath := userPath + "/" + req.FileId
+	fileUpload, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create test file: %w", err)
+	}
+	defer fileUpload.Close()
+
+	// Escribir el contenido del archivo binario en el archivo
+	_, err = fileUpload.Write(req.BinaryFile)
+	if err != nil {
+		return fmt.Errorf("failed to write binary content to file: %w", err)
+	}
+
+	return nil
+}
