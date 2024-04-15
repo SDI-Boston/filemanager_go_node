a6c991a846024d2d140fee6ebf2f4a694d3ffd93
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-15 18:35:52 -0500

    Update NFS mount address in docker-compose.yaml

---
+++ serverConf.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y nfs-kernel-server nfs-common
+sudo systemctl start nfs-kernel-server.service
+sudo mkdir -p /var/nfs/files
+sudo chown nobody:nogroup /var/nfs/files
+#NFS exports conf
+#/var/nfs/files *(rw,sync,no_subtree_check,no_root_squash)
