abfcd31079f16472744ed2e578f34c2cb59ef27e
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-15 19:40:57 -0500

    cliente de prueba

--- client/grpc/grpcClient.go
+++ client/grpc/grpcClient.go
@@ -13,7 +13,7 @@ import (
 )
 
 func UploadClientFile() {
-	serverAddr := "172.171.240.20:5000"
+	serverAddr := "node.eastus.cloudapp.azure.com:5000"
 	filePath := "./grpc.txt"
 	ownerID := "owner1"
 
