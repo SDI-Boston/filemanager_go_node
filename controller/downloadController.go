0aa5fe01abd7235676064167ebdcbd4ddac033ed
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-20 16:19:00 -0500

    fix descargas (test)

--- controller/downloadController.go
+++ controller/downloadController.go
@@ -6,38 +6,42 @@ import (
 	"net/http"
 	"os"
 	"github.com/gorilla/mux"
+	"path/filepath"
+	"mime"
 )
 
 // DownloadFileHandler descarga un archivo por su ID
 func DownloadFileHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	fileID := vars["fileId"]
-	userID := vars["userId"]
-	filePath := fmt.Sprintf("/mnt/nfs/%s/%s", userID, fileID)
+    vars := mux.Vars(r)
+    fileID := vars["fileId"]
+    userID := vars["userId"]
+    filePath := fmt.Sprintf("/mnt/nfs/%s/%s", userID, fileID)
 
-	file, err := os.Open(filePath)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer file.Close()
+    file, err := os.Open(filePath)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer file.Close()
 
-	// Obtener el tipo de contenido del archivo basado en su contenido
-	contentType := http.DetectContentType(nil)
+    // Obtener la extensión del archivo
+    fileExt := filepath.Ext(fileID)
 
-	// Establecer el tipo de contenido adecuado
-	if contentType == "" {
-		contentType = "application/octet-stream"
-	}
+    // Establecer el tipo de contenido basado en la extensión del archivo
+    contentType := mime.TypeByExtension(fileExt)
+    if contentType == "" {
+        contentType = "application/octet-stream"
+    }
 
-	// Set appropriate headers
-	w.Header().Set("Content-Disposition", "attachment; filename="+fileID)
-	w.Header().Set("Content-Type", contentType)
+    // Establecer los encabezados de respuesta
+    w.Header().Set("Content-Disposition", "attachment; filename="+fileID)
+    w.Header().Set("Content-Type", contentType)
 
-	// Copiar el archivo al escritor de respuesta
-	_, err = io.Copy(w, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    // Copiar el archivo al escritor de respuesta
+    _, err = io.Copy(w, file)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
+
