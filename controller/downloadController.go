b72309a498ec18799144dd8c6edfa5ba302d425d
v4ld1 <dzvaldiviesod@gmail.com>
2024-04-20 16:14:31 -0500

    descargar archivo con su tipo (test)

--- controller/downloadController.go
+++ controller/downloadController.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-
 	"github.com/gorilla/mux"
 )
 
@@ -23,10 +22,19 @@ func DownloadFileHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	// Obtener el tipo de contenido del archivo basado en su contenido
+	contentType := http.DetectContentType(nil)
+
+	// Establecer el tipo de contenido adecuado
+	if contentType == "" {
+		contentType = "application/octet-stream"
+	}
+
 	// Set appropriate headers
 	w.Header().Set("Content-Disposition", "attachment; filename="+fileID)
+	w.Header().Set("Content-Type", contentType)
 
-	// Copy the file to the response writer
+	// Copiar el archivo al escritor de respuesta
 	_, err = io.Copy(w, file)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
