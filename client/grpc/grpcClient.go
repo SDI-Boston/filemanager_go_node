f432dd36d2797cf5e882725bb6c566828ade51a3
Your Name <you@example.com>
2024-04-24 22:29:57 -0500

    Update server address and port number in nginx.conf

--- client/grpc/grpcClient.go
+++ client/grpc/grpcClient.go
@@ -12,7 +12,6 @@ import (
 
 	pb "github.com/SDI-Boston/filemanager_go_node/client/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func UploadClientFile() {
@@ -28,7 +27,7 @@ func UploadClientFile() {
 	conn, err := grpc.DialContext(
 		ctx,
 		serverAddr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithInsecure(), // Usar conexión insegura para NGINX
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024*1024*1024)),
 	)
 	if err != nil {
@@ -38,49 +37,49 @@ func UploadClientFile() {
 
 	client := pb.NewFileServiceClient(conn)
 
-	// Read the content of the file
+	// Leer el contenido del archivo
 	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		log.Fatalf("Failed to read file: %v", err)
 	}
 
-	// Encode the file content to base64
+	// Codificar el contenido del archivo a base64
 	encodedContent := base64.StdEncoding.EncodeToString(fileContent)
 
-	// Calculate the SHA256 hash of the file content
+	// Calcular el hash SHA256 del contenido del archivo
 	hash := sha256.New()
 	hash.Write(fileContent)
 	//fileHash := fmt.Sprintf("%x", hash.Sum(nil))
 
-	// Extract the file name with extension
+	// Extraer el nombre del archivo con extensión
 	fileName := filepath.Base(filePath)
 
-	// Create the upload request with the calculated hash
+	// Crear la solicitud de carga con el hash calculado
 	uploadRequest := &pb.FileUploadRequest{
 		FileId:     "99",
 		OwnerId:    ownerID,
 		BinaryFile: []byte(encodedContent),
 		FileName:   fileName,
-		//FileHash:   fileHash, // Include the calculated hash
+		//FileHash:   fileHash, // Incluir el hash calculado
 	}
 
-	// Open a stream to send the file
+	// Abrir un flujo para enviar el archivo
 	stream, err := client.Upload(ctx)
 	if err != nil {
 		log.Fatalf("Failed to open stream: %v", err)
 	}
 
-	// Send the upload request
+	// Enviar la solicitud de carga
 	if err := stream.Send(uploadRequest); err != nil {
 		log.Fatalf("Failed to send upload request: %v", err)
 	}
 
-	// Close the stream and receive the response
+	// Cerrar el flujo y recibir la respuesta
 	response, err := stream.CloseAndRecv()
 	if err != nil {
 		log.Fatalf("Failed to receive response: %v", err)
 	}
 
-	// Print the response and the array of URLs
+	// Imprimir la respuesta y la matriz de URLs
 	fmt.Printf("File uploaded successfully. Path: %s\n", response)
 }
